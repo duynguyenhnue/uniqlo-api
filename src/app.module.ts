2a62609739cfeb56d1da4f512c898f1b6d03ceae
milly.nguyen <milly.nguyen@qrra.com>
2024-08-22 11:53:39 +0700

    add mogodb srv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './api/user/user.module';
-import { AuthModule } from './api/auth/auth.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserModule } from "./api/user/user.module";
+import { AuthModule } from "./api/auth/auth.module";
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb:localhost:27017'),
+    MongooseModule.forRoot(
+      "mongodb+srv://stu715105064:ZeClf5THTVJyp5Jr@cluster0.yyq3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    ),
     UserModule,
     AuthModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
