fe818fcb96d8e52e80059684b913f21a25f4d22f
duynguyenhnue <stu715105064@hnue.edu.vn>
2024-09-21 18:10:54 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,3 @@
-import "dotenv/config";
-
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserModule } from "./api/users/users.module";
@@ -9,9 +7,14 @@ import { JwtAuthGuard } from "./common/guards/jwt-auth.gaurd";
 import { RefreshTokenModule } from "./api/refresh-token/refresh-token.module";
 import { RoleModule } from "./api/roles/role.module";
 import { PermissionModule } from "./api/permission/permission.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.DATABASE_MONGO_SRC),
     UserModule,
     AuthModule,
